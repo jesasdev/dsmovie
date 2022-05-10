067560662f28711982763ee94d30148d5e611b1e
Jesaias Santos <jesasdev@gmail.com>
2022-05-09 21:50:38 -0300

    Projeto criado

---
+++ BACKEND/src/test/java/com/js/dsmovie/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.js.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
